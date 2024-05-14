6040cf610449cb3ffc6243abd76fa707a41ca3db
Avinash Singh <113308043+AvinashKrishnaSingh@users.noreply.github.com>
2024-05-14 15:42:04 -0400

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// database parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', "");
+define('DB_NAME', 'serviceswift');
+
+//app parameters
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '/serviceswift');
+  // Site Name
+  define('SITENAME', 'Serviceswift');
+  //website url
+  define ('SITEURL','/serviceswift');
+
+  ?>
